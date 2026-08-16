e5163f23f9d0ec3cf15022e2d0c0e66393a4f4ce
Devon Vendetti <devdetti@gmail.com>
2026-08-15 19:56:07 -0700

    Adding level 5, 2 exercises

---
+++ lvl05x01_ThreeTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Dexter";
+char symbol = 'X';
+int number = 11;
+
+Console.WriteLine(name);
+Console.WriteLine(symbol);
+Console.WriteLine(number);
---
+++ lvl05x02_FirstCrash/Program.cs
@@ -0,0 +1,6 @@
+﻿string text = "Trouble";
+Console.WriteLine(text[0]);
+Console.WriteLine(text[1]);
+Console.WriteLine(text[4]);
+
+Console.WriteLine(text[6]);
