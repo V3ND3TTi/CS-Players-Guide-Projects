a4a4f3cf45cc1862325b0d113206a22d8ec6d897
Devon Vendetti <devdetti@gmail.com>
2026-08-15 18:41:34 -0700

    Added lvl 4 exercise 1

---
+++ lvl04x01_ReusingAVariable/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your symbol of choice: ");
+string symbol = Console.ReadLine()!;
+
+Console.WriteLine($"{symbol} {symbol}");
+Console.WriteLine($" {symbol} ");
+Console.WriteLine($"{symbol} {symbol}");
+
+Console.Write("Please enter another symbol: ");
+symbol = Console.ReadLine()!;
+
+Console.WriteLine($"{symbol}.{symbol}..{symbol}");
