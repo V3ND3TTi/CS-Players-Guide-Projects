1b31433b8e9223949b37eb1b0506e0543e5de8b8
Devon Vendetti <devdetti@gmail.com>
2026-08-17 21:39:23 -0700

    added 2 challenges from lvl 8

---
+++ lvl08x01_Reset/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Please enter a symbol to sort the stones to the appropriate channel:");
+Console.WriteLine("^ - Channel 1");
+Console.WriteLine("x - Channel 2");
+Console.WriteLine("o - Channel 3");
+Console.WriteLine("# - Channel 4");
+Console.Write("Please enter your choice (^,x,0,#): ");
+char choice = char.Parse(Console.ReadLine()!);
+
+switch (choice)
+{
+    case '^':
+        Console.WriteLine("Routing to Channel 1");
+        break;
+    case 'x':
+        Console.WriteLine("Routing to Channel 2");
+        break;
+    case 'o':
+        Console.WriteLine("Routing to Channel 3");
+        break;
+    case '#':
+        Console.WriteLine("Routing to Channel 4");
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+}
---
+++ lvl08x02_MustGoFaster/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please enter a symbol to sort the stones to the appropriate channel:");
+Console.WriteLine("^ - Channel 1");
+Console.WriteLine("x - Channel 2");
+Console.WriteLine("o - Channel 3");
+Console.WriteLine("# - Channel 4");
+Console.Write("Please enter your choice (^,x,0,#): ");
+char choice = char.Parse(Console.ReadLine()!);
+
+string response = choice switch
+{
+    '^' => "Routing to Channel 1",
+    'x' => "Routing to Channel 2",
+    'o' => "Routing to Channel 3",
+    '#' => "Routing to Channel 4",
+    _ => "Invalid choice."
+};
+Console.WriteLine($"{response}");
