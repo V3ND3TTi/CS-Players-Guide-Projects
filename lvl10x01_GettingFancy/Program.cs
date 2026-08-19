c958604f6c00a85fcac0d09836dbd9c70b6688e8
Devon Vendetti <devdetti@gmail.com>
2026-08-18 21:47:06 -0700

    added 2 challenges from lvl 10

---
+++ lvl10x01_GettingFancy/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Title = "Demo";
+
+Console.Write("Please enter a number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter another number: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.WriteLine($"{num1} + {num2} = {num1 + num2}.");
+Console.ResetColor();
+Console.Beep();
+Console.Write("Press any key to quit...");
+Console.ReadKey(false);
+Console.Clear();
---
+++ lvl10x02_CoffeeMachine/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int time = 4;
+int cream = 3;
+int r = 75 + 30 * cream;
+int g = 54 + 23 * cream;
+int b = 33 + 19 * cream;
+
+string italicsOn = "\e[3m";
+string italicsOff = "\e[23m";
+string setForeground = "\e[38;2;240;227;144m";
+string resetForeground = "\e[39m";
+string setBackground = $"\e[48;2;{r};{g};{b}m";
+string resetBackground = "\e[49m";
+
+Thread.Sleep(time * 1000);
+Console.WriteLine($"The {italicsOn}{setForeground}{setBackground}☕coffee{italicsOff}{resetForeground}{resetBackground} is ready!");
