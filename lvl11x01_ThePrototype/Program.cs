dab0752aabcd4f4267310c418424a1513b85e665
Devon Vendetti <devdetti@gmail.com>
2026-08-18 23:26:24 -0700

    added 2 challenges from lvl 11

---
+++ lvl11x01_ThePrototype/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter a number (1-100): ");
+int number = int.Parse(Console.ReadLine()!);
+int guess;
+
+Console.Clear();
+Console.Write("Guess the number between 1 and 100: ");
+
+while (true)
+{
+    do
+    {
+        guess = int.Parse(Console.ReadLine()!);
+        if (guess < 0 || guess > 100) Console.Write("Invalid input. Pick another number: ");
+
+    } while (guess < 0 || guess > 100);
+
+    if (guess == number) break;
+    if (guess > number) Console.Write("Too high! Pick a lower number: ");
+    if (guess < number) Console.Write("Too low! Pick a higher number: ");
+}
+
+Console.WriteLine("Congrats! You picked the correct number!");
---
+++ lvl11x02_Heartbeats/Program.cs
@@ -0,0 +1,26 @@
+﻿for (var i = 1; i <= 100; i++)
+{
+   var heartbeat = (i % 3, i % 5) switch
+   {
+      (0, 0) => "OO",
+      (0, _) => "o.",
+      (_, 0) => ".o",
+      _ => ".."
+   };
+   Console.WriteLine(heartbeat);
+}
+
+
+/* using if statements
+for (var i = 1; i <= 100; i++)
+{
+   if (i % 3 == 0 && i % 5 == 0)
+      Console.WriteLine("OO");
+   else if (i % 3 == 0)
+      Console.WriteLine("o.");
+   else if (i % 5 == 0)
+      Console.WriteLine(".o");
+   else
+      Console.WriteLine("..");
+}
+*/
