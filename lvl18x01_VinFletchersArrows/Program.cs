1563164a0ebcf3a86e2ef0797312ac85b90de75f
Devon Vendetti <devdetti@gmail.com>
2026-08-25 22:17:04 -0700

    Added 2 challenges from lvl 18

---
+++ lvl18x01_VinFletchersArrows/Program.cs
@@ -0,0 +1,50 @@
+﻿var arrow = new Arrow
+{
+    Arrowhead = Arrowhead.Broadhead,
+    Fletching = Fletching.Goose,
+    Weight = 35
+};
+
+arrow.DisplayArrow();
+
+class Arrow
+{
+    public Arrowhead Arrowhead;
+    public Fletching Fletching;
+    public float Weight;
+
+    public float GetDamage()
+    {
+        int arrowheadDamage = Arrowhead switch
+        {
+            Arrowhead.Broadhead => 8,
+            Arrowhead.Field => 5,
+            Arrowhead.Blunt => 3
+        };
+
+        int fletchingOffset = Fletching switch
+        {
+            Fletching.Goose => 0,
+            Fletching.Turkey => -1,
+            Fletching.Chicken => -2
+        };
+
+        return (arrowheadDamage + fletchingOffset) * Weight / 50;
+    }
+
+    public void DisplayArrow()
+    {
+        string arrowDisplay = $"""
+                               --------------------------
+                               Weight: {Weight}
+                               Arrowhead: {Arrowhead}
+                               Fletching: {Fletching} Feathers
+                               Damage: {GetDamage()}
+                               --------------------------
+                               """;
+        Console.WriteLine(arrowDisplay);
+    }
+}
+
+enum Arrowhead { Broadhead, Field, Blunt }
+enum Fletching { Goose, Turkey, Chicken }
---
+++ lvl18x02_ComparingArrows/Program.cs
@@ -0,0 +1,66 @@
+﻿var practiceArrow = new Arrow
+{
+    Arrowhead = Arrowhead.Blunt,
+    Fletching = Fletching.Chicken,
+    Weight = 30
+};
+
+var marksmanArrow = new Arrow
+{
+    Arrowhead = Arrowhead.Field,
+    Fletching = Fletching.Turkey,
+    Weight = 35
+};
+
+var eliteArrow = new Arrow
+{
+    Arrowhead = Arrowhead.Broadhead,
+    Fletching = Fletching.Goose,
+    Weight = 45
+};
+
+practiceArrow.DisplayArrow();
+marksmanArrow.DisplayArrow();
+eliteArrow.DisplayArrow();
+
+class Arrow
+{
+    public Arrowhead Arrowhead;
+    public Fletching Fletching;
+    public float Weight;
+
+    public float GetDamage()
+    {
+        int arrowheadDamage = Arrowhead switch
+        {
+            Arrowhead.Broadhead => 8,
+            Arrowhead.Field => 5,
+            Arrowhead.Blunt => 3
+        };
+
+        int fletchingOffset = Fletching switch
+        {
+            Fletching.Goose => 0,
+            Fletching.Turkey => -1,
+            Fletching.Chicken => -2
+        };
+
+        return (arrowheadDamage + fletchingOffset) * Weight / 50;
+    }
+
+    public void DisplayArrow()
+    {
+        string arrowDisplay = $"""
+                               --------------------------
+                               Weight: {Weight}
+                               Arrowhead: {Arrowhead}
+                               Fletching: {Fletching} Feathers
+                               Damage: {GetDamage()}
+                               --------------------------
+                               """;
+        Console.WriteLine(arrowDisplay);
+    }
+}
+
+enum Arrowhead { Broadhead, Field, Blunt }
+enum Fletching { Goose, Turkey, Chicken }
