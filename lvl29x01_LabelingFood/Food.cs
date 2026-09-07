14e8ebfa637b0909409bcd9c879e01371dc0f7df
Devon Vendetti <devdetti@gmail.com>
2026-09-06 17:53:20 -0700

    added 2 challenges from lvl 29 Polymorphism

---
+++ lvl29x01_LabelingFood/Food.cs
@@ -0,0 +1,47 @@
+﻿public class Food
+{
+    public string? Name { get; set; }
+    public int Points { get; set; }
+
+    public Food(string? name, int points)
+    {
+        Name = name;
+        Points = points;
+    }
+    public void Eat(Health health) => health.Hunger -= Points;
+
+    public override string ToString() => $"{Name}({Points})";
+}
+
+public class Cookie : Food
+{
+    public Cookie(string? name = "Cookie", int points = 2) : base(name, points) { }
+}
+
+public class Turkey : Food
+{
+    public Turkey(string? name = "Turkey", int points = 5) : base(name, points) { }
+}
+
+public class RawTurkey : Food
+{
+    public RawTurkey(string? name = "Raw Turkey", int points = 1) : base(name, points) { }
+}
+
+public class Milk : Food
+{
+    public Milk(string? name = "Milk", int points = 3) : base(name, points) { }
+}
+
+
+public class Health
+{
+    public int Hunger
+    {
+        get;
+        set { field = Math.Clamp(value, 0, 10); }
+    }
+    public Status Status { get; set; } = Status.None;
+}
+
+public enum Status { None, Sleepy, OnFire, Poisoned }
---
+++ lvl29x02_SideEffects/Program.cs
@@ -0,0 +1,29 @@
+﻿Health health = new();
+
+while (true)
+{
+    Console.WriteLine($"Hunger: {health.Hunger} Status: {health.Status}");
+    Console.WriteLine($"1=Cookie 2=Turkey 3=RawTurkey 4=Milk");
+    char input = Console.ReadKey(true).KeyChar;
+
+    Food? food = null;
+    food = input switch
+    {
+        '1' => new Cookie(),
+        '2' => new Turkey(),
+        '3' => new RawTurkey(),
+        '4' => new Milk(),
+        _ => null
+    };
+
+    if (food == null)
+    {
+        health.Hunger++;
+        Console.WriteLine("Ate nothing. Hunger has increased by 1.");
+    }
+    else
+    {
+        Console.WriteLine($"Eating {food}.");
+        food?.Eat(health);
+    }
+}
