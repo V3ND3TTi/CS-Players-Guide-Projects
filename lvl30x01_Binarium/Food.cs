cf49f88a1e007488220a14c7f99f8efba37e27fd
Devon Vendetti <devdetti@gmail.com>
2026-09-07 12:08:06 -0700

    added 1 challenge from lvl 30 interfaces

---
+++ lvl30x01_Binarium/Food.cs
@@ -0,0 +1,89 @@
+﻿public class Food : IConsumable
+{
+    public string? Name { get; set; }
+    public int Points { get; set; }
+
+    public Food(string? name, int points)
+    {
+        Name = name;
+        Points = points;
+    }
+    public virtual void Eat(Health health) => health.Hunger -= Points;
+
+    public override string ToString() => $"{Name} ({Points})";
+}
+
+public class Cookie : Food
+{
+    public Cookie(string? name = "Cookie", int points = 2) : base(name, points) { }
+
+    public override void Eat(Health health)
+    {
+        health.Hunger -= Points;
+        health.Status = Status.OnFire;
+    }
+}
+
+public class Turkey : Food
+{
+    public Turkey(string? name = "Turkey", int points = 5) : base(name, points) { }
+
+    public override void Eat(Health health)
+    {
+        health.Hunger -= Points;
+        health.Status = Status.Sleepy;
+    }
+}
+
+public class RawTurkey : Food
+{
+    public RawTurkey(string? name = "Raw Turkey", int points = 1) : base(name, points) { }
+
+    public override void Eat(Health health)
+    {
+        health.Hunger -= Points;
+        health.Status = Random.Shared.NextDouble() < 0.5 ? Status.Poisoned : health.Status;
+    }
+}
+
+public class Milk : Food
+{
+    public Milk(string? name = "Milk", int points = 3) : base(name, points) { }
+
+    public override void Eat(Health health)
+    {
+        health.Hunger -= Points;
+        health.Status = Status.None;
+    }
+}
+
+public class CodeDust : IConsumable
+{
+    public string? Name { get; private set; } = "Code Dust";
+
+    public void Eat(Health health)
+    {
+        if (health.Status == Status.OnFire) health.Binarium += .01f;
+    }
+
+    public override string ToString() => $"{Name}";
+}
+
+
+public class Health
+{
+    public int Hunger
+    {
+        get;
+        set { field = Math.Clamp(value, 0, 10); }
+    }
+    public Status Status { get; set; } = Status.None;
+    public float Binarium { get; set; } = 0.0f;
+}
+
+public interface IConsumable
+{
+    public void Eat(Health health);
+}
+
+public enum Status { None, Sleepy, OnFire, Poisoned }
