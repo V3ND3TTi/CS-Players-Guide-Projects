01ecf4b7e86c253578b72402c857cae77a807502
Devon Vendetti <devdetti@gmail.com>
2026-09-07 20:58:42 -0700

    added 1 challenge from lvl 32 records

---
+++ lvl32x01_WarPrep/Program.cs
@@ -0,0 +1,12 @@
+﻿var practiceSword = new Sword(Material.Iron, Gemstone.None, 100, 25);
+var battleSword = practiceSword with { Mat = Material.Steel, Gem = Gemstone.Sapphire };
+var binariumSword = practiceSword with { Mat = Material.Binarium, Gem = Gemstone.Bitstone };
+
+Console.WriteLine(practiceSword);
+Console.WriteLine(battleSword);
+Console.WriteLine(binariumSword);
+
+public record Sword(Material Mat, Gemstone Gem, int Length, int CrossguardWidth);
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gemstone { None, Emerald, Amber, Sapphire, Diamond, Bitstone }
