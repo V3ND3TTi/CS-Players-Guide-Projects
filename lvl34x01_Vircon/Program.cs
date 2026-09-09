e61b6e1892559a3698dbb44808feffae21eee572
Devon Vendetti <devdetti@gmail.com>
2026-09-08 21:05:15 -0700

    added 1 challenge from lvl 34 generics

---
+++ lvl34x01_Vircon/Program.cs
@@ -0,0 +1,23 @@
+﻿var virString = new Vircon<string>("Inception");
+Console.WriteLine(virString.Current);
+virString.Commit();
+Console.WriteLine(virString.Stored);
+virString.Current = "Tenet";
+Console.WriteLine(virString.Current);
+Console.WriteLine(virString.Stored);
+
+var virDouble = new Vircon<double>(3.14);
+Console.WriteLine(virDouble.Current);
+virDouble.Commit();
+Console.WriteLine(virDouble.Stored);
+virDouble.Current = 6.28;
+Console.WriteLine(virDouble.Current);
+
+public class Vircon<T>(T initial)
+{
+    public T Current { get; set; } = initial;
+    public T Stored { get; private set; }
+
+    public void Commit() => Stored = Current;
+    public void Revert() => Current = Stored;
+}
