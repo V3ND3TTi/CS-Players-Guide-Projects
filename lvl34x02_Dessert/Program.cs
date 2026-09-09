d48ef7846febef1d8dfdeb06c6eec8d52cf6d845
Devon Vendetti <devdetti@gmail.com>
2026-09-08 21:34:32 -0700

    added 2nd challenge from lvl 34 generics

---
+++ lvl34x02_Dessert/Program.cs
@@ -0,0 +1,11 @@
+﻿var ingredients = new List<string>() { "butter", "peaches", "honey", "vanilla", "cinnamon", "sugar", "flour", "salt", "milk" };
+
+while (ingredients.Count > 0)
+{
+    Console.Write("Enter the name of an ingredient: ");
+    var name = Console.ReadLine();
+    if (ingredients.Remove(name)) Console.WriteLine($"{name} removed from list and added to mixing bowl.");
+    else Console.WriteLine($"{name} not found.");
+}
+
+Console.WriteLine("You have listed all the ingredients. Peach cobbler is served!");
